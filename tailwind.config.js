fed012ab5648b2e19faea180bd991a27f3b34279
lgw <nihao3634@naver.com>
2025-01-20 14:29:08 +0900

    tailwind css rollback

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'naver-pastel': {
+          navy: '#5B6C8C',
+          blue: '#A4B7DD',
+          green: '#9ED6B7',
+          pink: '#FFD8E1',
+          yellow: '#FFE9B0',
+          gray: '#F5F6F7'
+        }
+      }
+    },
   },
   plugins: [],
 } 
